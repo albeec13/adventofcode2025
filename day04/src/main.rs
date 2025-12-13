9f048e64b3ff04964debf8289d7bbb4b974bb666
Albert Chaharbakhshi <albeec13@gmail.com>
2025-12-12 21:00:50 -0600

    Day 4, Puzzle 2, generic solution, full matrix, no moving window

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -21,107 +21,114 @@ impl CharSlice for str {
     }
 }
 
-struct RollingMatrix<T> {
-    rows: Vec<Vec<T>>,
-    max_rows: usize,
-    roll_indicator: T,
+#[derive(Clone)]
+struct RollMatrix {
+    rows: Vec<Vec<char>>,
+    roll_indicator: char,
 }
 
-impl<T: std::fmt::Display + std::cmp::PartialEq> RollingMatrix<T> {
-    fn new(roll_indicator: T) -> Self {
-        Self {rows: Vec::new(), max_rows: 3, roll_indicator}
+impl RollMatrix {
+    fn new(roll_indicator: char) -> Self {
+        Self {rows: Vec::new(), roll_indicator}
     }
 
-    fn push(&mut self, row: Vec<T>) {
-        if self.rows.len() == self.max_rows {
-            self.rows.remove(0);
-        }
+    fn push(&mut self, row: Vec<char>) {
         self.rows.push(row);
     }
 
-    fn check_at(&self, row: usize, i: usize, offset: isize) -> u64 {
-        i.checked_add_signed(offset)
-            .and_then(|idx| self.rows[row].get(idx))
-            .map(|c| (*c == self.roll_indicator) as u64)
+    fn check_at(&self, row_index: usize, row_offset: isize, col_index: usize, col_offset: isize) -> u64 {
+        row_index.checked_add_signed(row_offset)
+            .and_then(|r| self.rows.get(r))
+            .and_then(|row| {
+                col_index.checked_add_signed(col_offset)
+                .and_then(|c| row.get(c))
+            })
+            .map(|elem| (*elem == self.roll_indicator) as u64)
             .unwrap_or(0)
     }
 
-    fn count_movable_rolls(&self) -> u64 {
+    fn count_movable_rolls(&mut self, single: bool) -> u64 {
         let mut cnt: u64 = 0;
-        if self.rows.len() < self.max_rows {
-            return 0;
-        } else {
-            //check above and below 2nd row to see how many rolls surround each roll in that row and return total
-            for (i, roll) in self.rows[1].iter().enumerate() {
-                if roll != &self.roll_indicator {
-                    print!("{}", roll);
-                    continue;
-                } else {
-                    let mut near = 0;
-                    if  !self.rows[0].is_empty() {
-                        //check top-left
-                        near += self.check_at(0, i, -1);
+        let mut weights = self.clone();
 
-                        //check above
-                        near += self.check_at(0, i, 0);
+        
+        loop  {
+            let mut new_cnt = 0;
+            for (j, row) in self.rows.iter().enumerate() {
+                for (i, roll) in row.iter().enumerate() {
+                    if roll != &self.roll_indicator {
+                        //print!("{}", roll);
+                        continue;
+                    } else {
+                        let mut near = 0;
+                        
+                        near += self.check_at(j, -1, i, -1);    //check top-left
+                        near += self.check_at(j, -1, i, 0);     //check above
+                        near += self.check_at(j, -1, i, 1);     //check top-right                    
+                        near += self.check_at(j, 0, i, -1);     //check left
+                        near += self.check_at(j, 0, i, 1);      //check right
+                        near += self.check_at(j, 1, i, -1);     //check bottom-left
+                        near += self.check_at(j, 1, i, 0);      //check bottom
+                        near += self.check_at(j, 1, i, 1);      //check bottom-right
 
-                        //check top-right
-                        near += self.check_at(0, i, 1);
+                        weights.rows[j][i] = near.to_string().chars().nth(0).unwrap_or('0');
+                        //print!("{}",  roll);
                     }
-
-                    //check left
-                    near += self.check_at(1, i, -1);
-
-                    //check right
-                    near += self.check_at(1, i, 1);
-                    
-                    if !self.rows[2].is_empty() {
-                        //check bottom-left
-                        near += self.check_at(2, i, -1);
-
-                        //check bottom
-                        near += self.check_at(2, i, 0);
-
-                        //check bottom-right
-                        near += self.check_at(2, i, 1);
-                    }
-
-                    if near < 4 {
-                        cnt += 1;
-                    }
-                    print!("{}", near);
                 }
+                //println!();
             }
+            println!();
+        
+            for row in weights.rows.iter_mut() {
+                for col in row.iter_mut() {
+                    if col.is_numeric() {
+                        if col.to_digit(10).unwrap_or(u32::MAX) < 4 {
+                            *col = 'X';
+                            print!("{}", col);
+                            *col = '.';
+                            new_cnt += 1;
+                        } else {
+                            *col = '@';
+                            print!("{}", col);
+                        }
+                    } else {
+                        print!("{}", col);
+                    }
+                }
+                println!();
+            }
+            
+            cnt += new_cnt;
+            println!("Rolls removed: {}", new_cnt);
+
+            // Only run once for original day 1 puzzle, otherwise continue to modify matrix
+            if single || new_cnt == 0 {
+                break;
+            }
+
+            *self = weights.clone();
         }
-        println!();
+
         cnt
     }
 }
 
 fn main() {
     //process 3 rows at a time in a moving window, where initally the first row is null, and at the end the last row is null
-    let mut rm: RollingMatrix<char> = RollingMatrix::new('@');
+    let mut rm: RollMatrix = RollMatrix::new('@');
     let mut total_movable_rolls_part1 = 0;
-
-    // insert empty first row
-    rm.push(Vec::new());
+    let mut total_movable_rolls_part2 = 0;
     
     // insert rows from input, when we reach 3 total, begin checking for movable rolls at position 2 in the 3-row moving window
     if let Ok(lines) = read_lines("./input") {
         for row in lines.map_while(Result::ok) {            
             rm.push(row.chars().collect());
-            //println!("{} matrix size {}", row, rm.rows.len());
-
-            if rm.rows.len() == 3
-            {
-                total_movable_rolls_part1 += rm.count_movable_rolls();
-            }
         }
     }
 
-    // insert one more empty last row and check bottom row (before empty row) for movable rolls
-    rm.push(Vec::new());
-    total_movable_rolls_part1 += rm.count_movable_rolls();
-
+    total_movable_rolls_part1 += rm.count_movable_rolls(true);
     println!("Total movable rolls: {}", total_movable_rolls_part1);
+    
+    total_movable_rolls_part2 += rm.count_movable_rolls(false);
+    println!("Total movable rolls: {}", total_movable_rolls_part2);
 }
